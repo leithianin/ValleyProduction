b3708815f6c295869f79dd2f818895b7700b469b
Timothe <linkazer@gmail.com>
2022-02-01 16:47:02 +0100

    gitflow-feature-stash: AreaSystemTests

--- ValleyProduction/Assets/_Scripts/Area/AreaDisplays/ADI_VegetationDisplayer.cs
+++ ValleyProduction/Assets/_Scripts/Area/AreaDisplays/ADI_VegetationDisplayer.cs
@@ -11,12 +11,57 @@ public class ADI_VegetationDisplayer : AreaDisplay
         public List<TreeBehavior> toDisplay;
     }
 
-    [SerializeField] private List<VegetationDisplayData> vegetByValidScore;
+    //[SerializeField] private List<VegetationDisplayData> vegetByValidScore;
+    [SerializeField] private List<TreeBehavior> treesInArea;
+
+    [SerializeField] private List<int> treeToDisable;
+
     //[SerializeField] private List<InteractionSequence> possibleSequences;
+
+    [ContextMenu("Set Trees")]
+    private void SetTrees()
+    {
+        treesInArea = new List<TreeBehavior>();
+
+        Collider[] colliderInArea = Physics.OverlapBox(transform.position, Vector3.one * 12.5f);
+
+        for(int i = 0; i < colliderInArea.Length; i++)
+        {
+            if(colliderInArea[i].GetComponent<TreeBehavior>() != null)
+            {
+                treesInArea.Add(colliderInArea[i].GetComponent<TreeBehavior>());
+            }
+        }
+    }
+
+    public int score;
 
     public override void OnUpdateScore(int newScore)
     {
-        for (int i = 0; i < vegetByValidScore.Count; i++)
+        score = newScore;
+        int currentTreeIndex = 0;
+
+        for(int i = 0; i < treeToDisable.Count; i++)
+        {
+            if (i < newScore)
+            {
+                if (!treesInArea[currentTreeIndex].IsSet)
+                {
+                    treesInArea[currentTreeIndex].SetTree();
+                }
+                currentTreeIndex++;
+            }
+            else
+            {
+                if (treesInArea[currentTreeIndex].IsSet)
+                {
+                    treesInArea[currentTreeIndex].UnsetTree();
+                }
+                currentTreeIndex++;
+            }
+        }
+
+        /*for (int i = 0; i < vegetByValidScore.Count; i++)
         {
             if (i < newScore)
             {
@@ -38,6 +83,6 @@ public class ADI_VegetationDisplayer : AreaDisplay
                     }
                 }
             }
-        }
+        }*/
     }
 }
