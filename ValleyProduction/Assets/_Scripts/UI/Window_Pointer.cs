9a5fae92c7f66c9d29beae101377da89692a9512
AlexC <alexandre.courtin.76@gmail.com>
2022-02-07 17:33:19 +0100

    Test : Pointer

---
+++ ValleyProduction/Assets/_Scripts/UI/Window_Pointer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window_Pointer : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+}
