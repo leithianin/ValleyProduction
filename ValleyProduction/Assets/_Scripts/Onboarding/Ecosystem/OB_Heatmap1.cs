86457ec5cf1a53d0e924f0fa4c64ba83643c1469
AlexC <alexandre.courtin.76@gmail.com>
2022-02-24 11:42:07 +0100

    Merge : UI

--- ValleyProduction/Assets/_Scripts/Onboarding/Ecosystem/OB_Heatmap1.cs
+++ ValleyProduction/Assets/_Scripts/Onboarding/Ecosystem/OB_Heatmap1.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OB_Heatmap1 : OnBoarding
 {
     //Cet Onboarding doit aussi gérer le dézoom de la camera pour montrer toute la zone
+    public bool isClickOnHeatmap = false;
+
+    public UnityEvent OnClickHeatmapEvent;
 
     protected override void OnPlay()
     {
@@ -20,5 +24,14 @@ public class OB_Heatmap1 : OnBoarding
     {
         OnBoardingManager.OnClickHeatmapNoise -= OnClick;
         Over();
+    }
+
+    public void OnClickHeatmap()
+    {
+        if(!isClickOnHeatmap)
+        {
+            isClickOnHeatmap = true;
+            OnClickHeatmapEvent?.Invoke();
+        }
     }
 }
--- ValleyProduction/Assets/_Scripts/Onboarding/Path/OB_Tools.cs
+++ ValleyProduction/Assets/_Scripts/Onboarding/Path/OB_Tools.cs
@@ -24,7 +24,7 @@ public class OB_Tools : OnBoarding
 
     public void OnClickBuild()
     {
-        if(!isClickOnBuild)
+        if (!isClickOnBuild)
         {
             isClickOnBuild = true;
             OnClickBuildEvent?.Invoke();
