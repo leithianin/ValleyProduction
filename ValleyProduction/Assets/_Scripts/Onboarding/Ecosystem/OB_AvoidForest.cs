adfd692349bc7fe49d6c0ba835ce0b8b4250c975
AlexC <alexandre.courtin.76@gmail.com>
2022-03-01 15:48:06 +0100

    Add OB_Ecosystem : First part of the faking scene

--- ValleyProduction/Assets/_Scripts/Onboarding/Ecosystem/OB_AvoidForest.cs
+++ ValleyProduction/Assets/_Scripts/Onboarding/Ecosystem/OB_AvoidForest.cs
@@ -1,23 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OB_AvoidForest : OnBoarding
 {
+    public UnityEvent fakeBehavior;
     protected override void OnEnd()
     {
+        Debug.Log("END");
+        PathManager.CreatePathData();
+        StartCoroutine(Wait());
+
         //Cinematiquuuee
     }
 
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.1f);
+        PathManager.DestroyLineList();
+        yield return new WaitForSeconds(0.5f);
+        PlayFakeBehavior();
+    }
     protected override void OnPlay()
     {
-        OnBoardingManager.OnWaterMill += OnClick;
+        OnBoardingManager.OnWindMill += OnClick;
     }
 
     public void OnClick(bool condition)
     {
-        Debug.Log("WaterMill");
-        OnBoardingManager.OnWaterMill -= OnClick;
+        OnBoardingManager.OnWindMill -= OnClick;
         Over();
+    }
+
+    public void PlayFakeBehavior()
+    {
+        fakeBehavior?.Invoke();
     }
 }
