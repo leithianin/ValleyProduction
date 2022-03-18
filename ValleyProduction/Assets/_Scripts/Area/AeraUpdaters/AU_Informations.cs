83f1e3d5fe6228abe02ebf3003dc5965f599b8d6
Timothe <linkazer@gmail.com>
2022-03-18 10:47:31 +0100

    Add Structure Informations

--- ValleyProduction/Assets/_Scripts/Area/AeraUpdaters/AU_Informations.cs
+++ ValleyProduction/Assets/_Scripts/Area/AeraUpdaters/AU_Informations.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class AU_Informations : MonoBehaviour
 {
+    [SerializeField] private string structureName;
+    [SerializeField] private string structureDescription;
+
     public AreaUpdater AU_Sound;
     public AreaUpdater AU_Plant;
     public AreaUpdater AU_Polluter;
 
-    public float GetNoiseScore => AU_Sound.GetScore();
-    public float GetPlantScore => AU_Plant.GetScore();
-    public float GetPolluterScore => AU_Polluter.GetScore();
+    public string GetName => structureName;
+    public string GetDescription => structureDescription;
+
+    public float GetNoiseScore()
+    {
+        if(AU_Sound != null)
+        {
+            return AU_Sound.GetScore();
+        }
+        return 0;
+    }
+
+    public float GetPlantScore()
+    {
+        if (AU_Plant != null)
+        {
+            return AU_Plant.GetScore();
+        }
+        return 0;
+    }
+
+
+    public float GetPolluterScore()
+    {
+        if (AU_Polluter != null)
+        {
+            return AU_Polluter.GetScore();
+        }
+        return 0;
+    }
 }
