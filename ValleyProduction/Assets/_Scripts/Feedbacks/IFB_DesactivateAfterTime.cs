d388a5b79577dda94bb53507bc023a1743230f8a
TimotheHome <linkazer@gmail.com>
2022-03-19 14:19:57 +0100

    Fix pause menu on game pause

--- ValleyProduction/Assets/_Scripts/Feedbacks/IFB_DesactivateAfterTime.cs
+++ ValleyProduction/Assets/_Scripts/Feedbacks/IFB_DesactivateAfterTime.cs
@@ -6,6 +6,8 @@ public class IFB_DesactivateAfterTime : MonoBehaviour, IFeedbackPlayer
 {
     private float timer = 0;
 
+    [SerializeField] private bool isRealTimer = false;
+
     public void Play()
     {
         gameObject.SetActive(false);
@@ -14,11 +16,20 @@ public class IFB_DesactivateAfterTime : MonoBehaviour, IFeedbackPlayer
     public void Play(float integer)
     {
         timer = integer;
-        StartCoroutine(PlayAfter());
+        //StartCoroutine(PlayAfter()); CODE REVIEW
+        if(isRealTimer)
+        {
+            TimerManager.CreateRealTimer(timer, Play);
+        }
+        else
+        {
+            TimerManager.CreateGameTimer(timer, Play);
+        }
     }
 
     IEnumerator PlayAfter()
     {
+        Debug.Log("OK");
         yield return new WaitForSeconds(timer);
         Play();
     }
