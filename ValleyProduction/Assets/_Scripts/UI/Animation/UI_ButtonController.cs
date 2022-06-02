6cb316ffb4a87d8f96e86a279d7d2e408c816801
Timothe <linkazer@gmail.com>
2022-06-02 14:42:46 +0200

    Some UI fixes

---
+++ ValleyProduction/Assets/_Scripts/UI/Animation/UI_ButtonController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UI_ButtonController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private Button linkedButton;
+
+    private bool isSelected = false;
+
+    public void SetBool(string boolName)
+    {
+        animator.SetBool(boolName, true);
+    }
+
+    public void UnsetBool(string boolName)
+    {
+        animator.SetBool(boolName, false);
+    }
+
+    public void SetSelected()
+    {
+        isSelected = !isSelected;
+
+        if(isSelected)
+        {
+            animator.SetBool("Selected", true);
+        }
+        else
+        {
+            animator.SetBool("Selected", false);
+        }
+    }
+}
