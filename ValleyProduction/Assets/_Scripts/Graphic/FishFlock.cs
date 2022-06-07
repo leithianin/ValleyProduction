a24c927441fb4724367774235c0d3786e9388e74
leith <m.lukas@rubika-edu.com>
2022-06-07 14:40:04 +0200

    Fishes wip

--- ValleyProduction/Assets/_Scripts/Graphic/FishFlock.cs
+++ ValleyProduction/Assets/_Scripts/Graphic/FishFlock.cs
@@ -8,36 +8,48 @@ public class FishFlock : MonoBehaviour
     public static int tankSize = 5;
 
     public bool patrol;
+    public Transform[] patrolPoints;
 
     static int numFish = 10;
     public static GameObject[] fishes = new GameObject[numFish];
 
     public static Vector3 goalPos = Vector3.zero;
 
+    [SerializeField] private Vector2 refreshPosRandom;
+
+    TimerManager.Timer refreshTargetTimer = null;
+
+    private Vector3 center;
+
     void Start()
     {
+        center = this.transform.position;
+
         for (int i = 0; i < numFish; i++)
         {
             Vector3 pos = new Vector3
             (
-                Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize)
+                Random.Range(center.x - tankSize, center.x + tankSize),
+                Random.Range(center.y - tankSize, center.y + tankSize),
+                Random.Range(center.z - tankSize, center.z + tankSize)
             );
-            fishes[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+            fishes[i] = Instantiate(fishPrefab, pos, Quaternion.identity, gameObject.transform);
         }
+
+        RefreshTarget();
     }
 
-    void Update()
+    private void RefreshTarget()
     {
-        if(Random.Range(0, 10000) < 50)
-        {
-            goalPos = new Vector3
-            (
-                Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize)
-            );
-        }
+        center = this.transform.position;
+
+        goalPos = new Vector3
+        (
+            Random.Range(center.x - tankSize, center.x + tankSize),
+            Random.Range(center.y - tankSize, center.y + tankSize),
+            Random.Range(center.z - tankSize, center.z + tankSize)
+        );
+
+        TimerManager.CreateGameTimer(Random.Range(refreshPosRandom.x, refreshPosRandom.y), RefreshTarget);
     }
 }
