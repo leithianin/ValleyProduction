6377e6dbbe54ab14c26235fcb978d16127905640
LinKazer <linkazer@gmail.com>
2022-06-19 18:15:30 +0200

    Fix disable GameObject on TIL scene

--- ValleyProduction/Assets/_Scripts/TreeBehavior/TreeMeshSetter.cs
+++ ValleyProduction/Assets/_Scripts/TreeBehavior/TreeMeshSetter.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class TreeMeshSetter : MonoBehaviour
 {
     [SerializeField] private TreeBehavior[] trees;
+
+    [SerializeField] private List<Transform> parrentToActivate;
 
 #if UNITY_EDITOR
     [ContextMenu("Get All trees")]
@@ -19,6 +22,28 @@ public class TreeMeshSetter : MonoBehaviour
         foreach(TreeBehavior t in trees)
         {
             t.GetAllMeshes();
+        }
+    }
+
+    [ContextMenu("Set all objects")]
+    public void EnableAllObjects()
+    {
+        foreach(Transform tr in parrentToActivate)
+        {
+            ActivateAllChildren(tr);
+        }
+    }
+
+    private void ActivateAllChildren(Transform parent)
+    {
+        foreach(Transform tr in parent)
+        {
+            tr.gameObject.SetActive(true);
+            if(tr.childCount > 0)
+            {
+                ActivateAllChildren(tr);
+            }
+            EditorUtility.SetDirty(tr.gameObject);
         }
     }
 #endif
